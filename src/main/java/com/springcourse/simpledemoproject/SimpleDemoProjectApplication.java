b494d45adb305aa0d70d04693a941ed6cdd81efc
Zhenya <zsydun@gamil.com>
2024-09-25 13:18:07 +0300

    first commit

---
+++ src/main/java/com/springcourse/simpledemoproject/SimpleDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springcourse.simpledemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcourse/simpledemoproject/SimpleDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcourse.simpledemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
